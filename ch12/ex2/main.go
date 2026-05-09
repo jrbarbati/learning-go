a5164ceecd0cf9c431acff2732204bfad1b920c8
Joseph Barbati <josephbarbati@Josephs-MacBook-Air.local>
2026-05-09 12:34:32 -0400

    concurrency ex2

--- ch12/ex2/main.go
+++ ch12/ex2/main.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	process()
+}
+
+func process() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	go func() {
+		defer close(ch1)
+		for i := 1; i <= 10; i++ {
+			ch1 <- i
+		}
+	}()
+
+	go func() {
+		defer close(ch2)
+		for i := 11; i <= 20; i++ {
+			ch2 <- i
+		}
+	}()
+
+	for ch1 != nil && ch2 != nil {
+		select {
+		case num1, ok := <-ch1:
+			if !ok {
+				ch1 = nil
+				continue
+			}
+			fmt.Println(num1)
+		case num2, ok := <-ch2:
+			if !ok {
+				ch2 = nil
+				continue
+			}
+			fmt.Println(num2)
+		}
+	}
+}
