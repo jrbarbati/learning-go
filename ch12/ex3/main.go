5b90c1152e0129689bc260b57d32d7941bb6da4a
Joseph Barbati <josephbarbati@Josephs-MacBook-Air.local>
2026-05-09 12:46:04 -0400

    concurrency ex3

--- ch12/ex3/main.go
+++ ch12/ex3/main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"fmt"
+	"math"
+	"sync"
+)
+
+const (
+	size = 100_000
+)
+
+var (
+	sqrtsMap = sync.OnceValue(buildMap)
+)
+
+func main() {
+	for i := 0; i < size; i += 1000 {
+		fmt.Println("sqrt of", i, "->", sqrtsMap()[i])
+	}
+}
+
+func buildMap() map[int]float64 {
+	m := make(map[int]float64, size)
+
+	for i := 0; i < size; i++ {
+		m[i] = math.Sqrt(float64(i))
+	}
+
+	return m
+}
