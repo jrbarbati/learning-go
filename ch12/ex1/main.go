b19037f4eed157c1634336b485465ee1252d5e3b
Joseph Barbati <josephbarbati@Josephs-MacBook-Air.local>
2026-05-09 12:20:10 -0400

    concurrency ex1

--- ch12/ex1/main.go
+++ ch12/ex1/main.go
@@ -1,1 +1,49 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	numCh := make(chan int)
+	wwg := sync.WaitGroup{}
+	rwg := sync.WaitGroup{}
+
+	wwg.Add(2)
+
+	go func() {
+		writeNums(1, 10, numCh)
+		wwg.Done()
+	}()
+
+	go func() {
+		writeNums(11, 20, numCh)
+		wwg.Done()
+	}()
+
+	go func() {
+		wwg.Wait()
+		close(numCh)
+	}()
+
+	rwg.Add(1)
+	go func() {
+		readNums(numCh)
+		rwg.Done()
+	}()
+
+	rwg.Wait()
+}
+
+func writeNums(start, finish int, ch chan<- int) {
+	for i := start; i <= finish; i++ {
+		ch <- i
+	}
+}
+
+func readNums(ch <-chan int) {
+	for num := range ch {
+		fmt.Println(num)
+	}
+}
